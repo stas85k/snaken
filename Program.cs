339497f9a18b7d9100053301a1f4bf0047ac36b4
stas85k <stas85k@gmail.com>
2018-01-03 12:00:02 +0300

    procedure

--- Program.cs
+++ Program.cs
@@ -6,8 +6,18 @@ namespace snaken
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Draw(x1,y1,sym1);
         }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
     }
 }
