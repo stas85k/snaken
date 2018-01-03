578b1b225d453766506709300008c87499302f5d
stas85k <stas85k@gmail.com>
2018-01-03 12:00:02 +0300

    lesson3 class

---
+++ Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace snaken
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point(int v1, int v2, char v3)
+        {
+            this.x = v1;
+            this.y = v2;
+            this.sym = v3;
+        }
+
+        public void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,8 +6,16 @@ namespace snaken
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Point p1 = new Point(1, 3, '*');
+
+            p1.Draw(p1.x, p1.y, p1.sym);
+
+            Point p2 = new Point(4, 5, '#');
+             
+            p2.Draw(p2.x, p2.y, p2.sym);
         }
+
+      
+
     }
 }
